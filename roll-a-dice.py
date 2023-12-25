e8290d4682be25b89b2aee6d07679280d9a3dc98
Akshaj111 <76700912+Akshaj111@users.noreply.github.com>
2023-12-25 15:08:31 +0000

    Add files via upload

--- roll-a-dice.py
+++ roll-a-dice.py
@@ -1,13 +1,11 @@
 import random
 import os 
-
+import 
 response = input("Press y if you want to roll the die, n if not.")
 no = random.randint(1,6)
 
-while True:
-    if response == 'y':
-        print(no)
-    elif response == 'n':
-        os.system('cls')
-        print("Thanks for trying out the die rolling simulator!")
-        break
+if response == 'y':
+    print(no)
+elif response == 'n':
+    os.system('cls')
+    print("Thanks for trying out the die rolling simulator!")
